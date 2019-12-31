2916af2a9ed2e8f2e41dbd795794a57b6c412a9c
alexcambose <alexcambose@yahoo.com>
2019-12-31 02:04:29 +0200

    Brightness module

---
+++ server/modules/brightness.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define INCREMENT "inc"
+#define DECREMENT "dec"
+#define BRIGHTNESS_FILE "/sys/class/backlight/intel_backlight/brightness"
+
+
+void main(int argc, char *argv[]) {
+    if(!argv[1]) {
+        printf("Usage: brightnes <%s|%s> \n", INCREMENT, DECREMENT);
+        exit(1);
+    }
+    FILE *fp = fopen(BRIGHTNESS_FILE, "r");
+    char buff[255];
+    fscanf(fp, "%s", buff);
+    int currentBrightness = atoi(buff);
+    printf("Current brightness is %d\n", currentBrightness);
+
+    if(strcmp(argv[1], INCREMENT) == 0) {
+        currentBrightness+=20;
+    } else if (strcmp(argv[1], DECREMENT) == 0) {
+        currentBrightness-=20;
+    } else {
+        printf("Argument %s not recognized", argv[1]);
+    }
+    sprintf(buff,"%d", currentBrightness);
+
+    printf("New brightness is %d\n", currentBrightness);
+    FILE *fpr = fopen(BRIGHTNESS_FILE, "w");
+    fputs(buff, fpr);
+    fclose(fp);
+}
